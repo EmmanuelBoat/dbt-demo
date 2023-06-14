504c26a4bf82365e0a90700b0675e71ca16c04a4
Emmanuel Boateng Sifah <emmanuelsifah@yahoo.com>
2023-06-14 21:51:07 +0000

    Added stg_payments for payments data, modification for fct_orders and dim_customers table

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,17 +1,30 @@
-with orders as (
-    select * from {{ ref('stg_orders') }}
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
 payments as (
     select * from {{ ref('stg_payments') }}
 ),
 
-fct_orders as (
-    select order_id,
-           payment_id,
-           amount
-    from orders o
-    left join payments p on o.order_id = p.orderid  
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-select * from fct_orders
+select * from final
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,13 +1,10 @@
-with payments as (
+select id as payment_id,
+       orderid as order_id,
+       paymentmethod as payment_method,
+       status,
 
-    select
-        id as payment_id,
-        orderid,
-        paymentmethod,
-        status,
-        amount,
-        created
-    from raw.stripe.payment
-)
+       -- amount is stored in cents, convert it to dollars
+       amount / 100 as amount,
+       created as created_at
 
-select * from payments
+from raw.stripe.payment 
